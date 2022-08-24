e5924ec4402ca44a2ed1cbcc1567847a71619aa3
brian <erinbeech@icloud.com>
2022-08-24 17:37:35 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+let reqBody = {
+    name: "erin",
+    email: "erin@erin.com"
+  };
+   function submitData(reqBody) { 
+ 
+    fetch("http://localhost:3000/users",{
+     method: "POST",
+     headers: {
+       "Content-Type": "application/json",
+       "Accept": "application/json"
+     },
+     body: JSON.stringify(submitData)
+    })
+   .then(resp => resp.json())
+    .then(submitData =>
+     console.log(submitData))
+    .catch(function(error) {
+          alert("Bad things! Ragnarők!");
+          console.log(error.message);
+    } 
